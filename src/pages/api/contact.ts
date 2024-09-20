0f8ff7e9dfa3bf7df3b53afe2fce2d383e13e835
romelgomez <bmxandcode@gmail.com>
2024-09-20 17:43:00 -0500

    chore: complet form example

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from 'astro';
 import { contactSchema } from '../../schemas/contact';
 
-export const post: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({ request }) => {
   try {
     const data = await request.json();
 

dfd6e7c47a3af51183b3ba54c6ed5110f7ac9fcd
romelgomez <bmxandcode@gmail.com>
2024-09-23 16:14:37 -0500

    feat: add example of auth with clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
-
+import clerk from '@clerk/astro'
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon()],
+  integrations: [react(), icon(), clerk()],
   output: 'server',
   adapter: netlify(),
 });

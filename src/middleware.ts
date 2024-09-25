e8955629f7c370540497541879acca2b923c3c7c
romelgomez <bmxandcode@gmail.com>
2024-09-25 18:53:52 -0500

    test: debug clerk - 003

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,14 +7,16 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/astro/server';
 
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 
-export const onRequest = clerkMiddleware((auth, context) => {
+export const onRequest = clerkMiddleware((auth, context, next) => {
   const { redirectToSignIn, userId } = auth();
 
   if (!userId && isProtectedRoute(context.request)) {
     // Add custom logic to run before redirecting
 
     return redirectToSignIn({
-      returnBackUrl: '/'
+      returnBackUrl: '/',
     });
   }
+
+  return next();
 });

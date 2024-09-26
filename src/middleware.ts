1d9010ec5ecc1c8a2e90554c1283336337b5e4a5
romelgomez <bmxandcode@gmail.com>
2024-09-25 20:37:55 -0500

    test: debug clerk - 010

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,20 @@
 // ref: https://clerk.com/docs/references/astro/clerk-middleware#protect-routes-based-on-user-authentication-status
 //
 
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/astro/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/astro/server';
 
-// const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 
-// export const onRequest = clerkMiddleware((auth, context, next) => {
-//   const { redirectToSignIn, userId } = auth();
+export const onRequest = clerkMiddleware((auth, context, next) => {
+  const { redirectToSignIn, userId } = auth();
 
-//   if (!userId && isProtectedRoute(context.request)) {
-//     // Add custom logic to run before redirecting
+  if (!userId && isProtectedRoute(context.request)) {
+    // Add custom logic to run before redirecting
 
-//     return redirectToSignIn({
-//       returnBackUrl: '/'
-//     });
-//   }
+    return redirectToSignIn({
+      returnBackUrl: '/'
+    });
+  }
 
-//   return next()
-// });
+  return next()
+});

8fcc13ecdd53c5b6350d999edb905980beb5aae3
romelgomez <bmxandcode@gmail.com>
2024-09-25 20:37:08 -0500

    Revert "test: debug clerk - 005"
    This reverts commit 6d8ac190da0ca63453fee7f35795d5e6e67f9951.

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+// Protect routes
+//
+// ref: https://clerk.com/docs/references/astro/clerk-middleware#protect-routes-based-on-user-authentication-status
+//
+
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/astro/server';
+
+// const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
+
+// export const onRequest = clerkMiddleware((auth, context, next) => {
+//   const { redirectToSignIn, userId } = auth();
+
+//   if (!userId && isProtectedRoute(context.request)) {
+//     // Add custom logic to run before redirecting
+
+//     return redirectToSignIn({
+//       returnBackUrl: '/'
+//     });
+//   }
+
+//   return next()
+// });

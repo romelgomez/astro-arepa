b01934c3d9102f04c0dda8eb433591f7865dc329
romelgomez <bmxandcode@gmail.com>
2024-09-27 10:56:45 -0500

    chore: save avances with the delete opt

--- src/components/posts/columns.tsx
+++ src/components/posts/columns.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+// import { useToast } from '@/hooks/use-toast';
 import type { ColumnDef } from '@tanstack/react-table';
 
 // Define the type for your posts
@@ -36,6 +37,45 @@ export const columns: ColumnDef<Post>[] = [
     header: 'Actions',
     cell: ({ row }) => {
       const postId = row.original.id;
+      // const { toast } = useToast();
+
+      // Function to handle the delete action
+      // const handleDelete = async () => {
+      //   const confirmed = confirm('Are you sure you want to delete this post?');
+      //   if (!confirmed) return;
+
+      //   try {
+      //     const response = await fetch(
+      //       `http://localhost:3000/api/v1/post/${postId}`,
+      //       {
+      //         method: 'DELETE',
+      //       },
+      //     );
+
+      //     if (response.ok) {
+      //       toast({
+      //         title: 'Success',
+      //         description: 'Post deleted successfully!',
+      //       });
+      //       // Optionally, trigger a refetch of your data here to update the table
+      //       window.location.reload(); // Refresh the page to update the data (or refetch data in a more optimized way)
+      //     } else {
+      //       const errorData = await response.json();
+      //       toast({
+      //         title: 'Error',
+      //         description: errorData.message || 'Failed to delete post',
+      //         variant: 'destructive',
+      //       });
+      //     }
+      //   } catch (error) {
+      //     toast({
+      //       title: 'An unexpected error occurred.',
+      //       description: 'Please try again later.',
+      //       variant: 'destructive',
+      //     });
+      //   }
+      // };
+
       return (
         <div className='flex space-x-2'>
           <a href={`/posts/view/${postId}`}>
@@ -48,6 +88,12 @@ export const columns: ColumnDef<Post>[] = [
               Edit
             </Button>
           </a>
+          {/* <Button variant='destructive' size='sm' onClick={handleDelete}>
+            Delete
+          </Button> */}
+          {/* <Button variant='destructive' size='sm' onClick={handleToast}>
+            toast
+          </Button> */}
         </div>
       );
     },
